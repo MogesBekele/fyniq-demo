84731ff2cbf4be72e8555d1e5fed62c1ff75c8ea
MogesBekele <mogesbekele32@gmail.com>
2025-08-30 17:34:36 +0300

    fix the alinment

--- frontend/src/components/dashboard/StaffDashboard.jsx
+++ frontend/src/components/dashboard/StaffDashboard.jsx
@@ -92,7 +92,7 @@ export default function StaffDashboard() {
           </a>
           <button
             onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg shadow transition"
+           className="bg-gray-200 hover:bg-gray-300 text-gray-700 px-4 py-2 rounded-lg shadow transition hover:cursor-pointer"
           >
             Logout
           </button>
