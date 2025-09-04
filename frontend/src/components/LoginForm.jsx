168c5b9ceb505a2078d7ca90b93906cbacceb5ca
MogesBekele <mogesbekele32@gmail.com>
2025-09-04 07:36:05 +0300

    fix import issue

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
-
 import { useNavigate } from "react-router-dom";
-
 import { useAuth } from "../context/useAuth";
 
 export default function AuthPage() {
--- frontend/src/components/dashboard/ClientDashboard.jsx
+++ frontend/src/components/dashboard/ClientDashboard.jsx
@@ -1,58 +1,3 @@
-// import { useState } from "react";
-// import FileUpload from "../FileUpload";
-// import { useAuth } from "../../context/useAuth";
-
-// export default function ClientDashboard() {
-//   const [uploadedFiles, setUploadedFiles] = useState([]);
-//   const {logout} = useAuth();
-
-// const handleLogout=()=>{
-//   logout();
-//   window.location.href = "/"; // redirect to login page
-// }
-
-//   return (
-//     <div className="min-h-screen w-full bg-gray-100 flex flex-col">
-//       {/* Header */}
-//       <header className="w-full bg-white shadow px-10 py-4 mb-10  flex justify-between items-center sticky top-0 z-10">
-//         <h1 className="text-2xl md:text-3xl font-bold text-gray-700">
-//           Client Dashboard
-//         </h1>
-//         <button
-//           onClick={handleLogout}
-//           className="bg-gray-200 hover:bg-gray-400 text-gray-800 px-3 py-1 rounded-lg shadow transition hover:cursor-pointer"
-//         >
-//           Logout
-//         </button>
-//       </header>
-
-//       {/* File Upload Section */}
-//       <FileUpload
-//         onUpload={(file) => setUploadedFiles((prev) => [...prev, file])}
-//       />
-
-//       {/* Uploaded Files */}
-//       {uploadedFiles.length > 0 && (
-//         <div className="m-20">
-//           {uploadedFiles.map((file) => (
-//             <div
-//               key={file.id}
-//               className=" justify-between flex border border-gray-200 rounded-lg p-4 bg-gray-50 shadow-sm hover:shadow-md transition"
-//             >
-//               <p className="font-medium text-gray-800 ">
-//                 📄 {file.filename}{" "}
-
-//               </p>
-//                   <button onClick={() => setUploadedFiles((prev) => prev.filter((f) => f.id !== file.id))}>Delete</button>
-//             </div>
-
-//           ))}
-
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
 
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -145,3 +90,71 @@ export default function ClientDashboard() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+// import FileUpload from "../FileUpload";
+// import { useAuth } from "../../context/useAuth";
+
+// export default function ClientDashboard() {
+//   const [uploadedFiles, setUploadedFiles] = useState([]);
+//   const {logout} = useAuth();
+
+// const handleLogout=()=>{
+//   logout();
+//   window.location.href = "/"; // redirect to login page
+// }
+
+//   return (
+//     <div className="min-h-screen w-full bg-gray-100 flex flex-col">
+//       {/* Header */}
+//       <header className="w-full bg-white shadow px-10 py-4 mb-10  flex justify-between items-center sticky top-0 z-10">
+//         <h1 className="text-2xl md:text-3xl font-bold text-gray-700">
+//           Client Dashboard
+//         </h1>
+//         <button
+//           onClick={handleLogout}
+//           className="bg-gray-200 hover:bg-gray-400 text-gray-800 px-3 py-1 rounded-lg shadow transition hover:cursor-pointer"
+//         >
+//           Logout
+//         </button>
+//       </header>
+
+//       {/* File Upload Section */}
+//       <FileUpload
+//         onUpload={(file) => setUploadedFiles((prev) => [...prev, file])}
+//       />
+
+//       {/* Uploaded Files */}
+//       {uploadedFiles.length > 0 && (
+//         <div className="m-20">
+//           {uploadedFiles.map((file) => (
+//             <div
+//               key={file.id}
+//               className=" justify-between flex border border-gray-200 rounded-lg p-4 bg-gray-50 shadow-sm hover:shadow-md transition"
+//             >
+//               <p className="font-medium text-gray-800 ">
+//                 📄 {file.filename}{" "}
+
+//               </p>
+//                   <button onClick={() => setUploadedFiles((prev) => prev.filter((f) => f.id !== file.id))}>Delete</button>
+//             </div>
+
+//           ))}
+
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
