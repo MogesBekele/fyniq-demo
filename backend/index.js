e2bfb6590ee793e3af98dea7f330e1da3f24935d
MogesBekele <mogesbekele32@gmail.com>
2025-09-04 08:49:23 +0300

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -9,21 +8,15 @@ import logRoutes from "./routes/logRoutes.js";
 
 
 dotenv.config(); // <-- load env variables
-connectDB();      // connect to MongoDB
+connectDB(); // connect to MongoDB
 
 const app = express();
-const allowedOrigins = process.env.NODE_ENV === "production"
-  ? ["https://fyniq-demo.vercel.app"] // your frontend URL in production
-  : ["http://localhost:5173"]; // your frontend URL in development
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(cors({ origin: "*" }));
+
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-console.log("JWT_SECRET =", process.env.JWT_SECRET);
 
 app.use("/auth", authRoutes);
 app.use("/api/files", fileRoutes);
