4054609957c20b99a4e3bc15f1a980919dc57c05
MogesBekele <mogesbekele32@gmail.com>
2025-09-04 10:53:10 +0300

    hidden

--- frontend/src/components/dashboard/StaffDashboard.jsx
+++ frontend/src/components/dashboard/StaffDashboard.jsx
@@ -83,7 +83,7 @@ export default function StaffDashboard() {
         <h1 className="text-2xl md:text-3xl font-bold text-gray-700">
           Staff Dashboard
         </h1>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 max-sm:hidden">
           <Link
           to="/logs"
             className="bg-gray-200 hover:bg-gray-300 text-gray-700 px-4 py-2 rounded-lg shadow transition"
