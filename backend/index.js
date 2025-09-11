bab20af0e14ae5ddd3050639a2e042cfbb59eaef
MogesBekele <mogesbekele32@gmail.com>
2025-09-11 08:16:40 +0300

    cors/issue

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import fileRoutes from "./routes/files.js";
 import connectDB from "./config/db.js";
 import logRoutes from "./routes/logRoutes.js";
 
-
 dotenv.config();
 connectDB();
 
@@ -14,14 +13,12 @@ const app = express();
 
 app.use(cors({ origin: "*" }));
 
-
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 app.use("/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 app.use("/api/logs", logRoutes);
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
